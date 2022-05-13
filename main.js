e7d450c82d8e765145fa00d36a55a3d276595539
Ploot-O <105445191+Ploot-O@users.noreply.github.com>
2022-05-13 16:01:54 -0400

    Update main.js
    version 0.12
    added progress saving
    added metaprestige

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ var aCost;
 var aUpgradeDiv;
 var prestigeDiv;
 var aPrestigeCost;
+var prestigeCountText;
+var aMetaprestigeCost;
+var metaprestigeCountText;
 //attach element variables to DOM elements
 aCount = document.getElementById("aCount");
 buttonDiv = document.getElementById("buttonDiv");
@@ -18,20 +21,46 @@ aCost = document.getElementById("aCost");
 aUpgradeDiv = document.getElementById("aUpgradeDiv");
 prestigeDiv = document.getElementById("prestigeDiv");
 aPrestigeCost = document.getElementById("aPrestigeCost");
+prestigeCountText = document.getElementById("prestigeCountText");
+aMetaprestigeCost = document.getElementById("aMetaprestigeCost");
+metaprestigeCountText = document.getElementById("metaprestigeCountText");
 
 //letters array
 var progress = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//major variables
-const letters = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0};
-var increaseAmount = 1;
-var currentLetter = 1;
-var upgradeCost = 25;
+var userData = {};
+//load userdata if available
+if(localStorage.getItem("gameStateData") === null){
+    //create new user data
+    userData = {
+        letters: {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0},
+        increaseAmount: 1,
+        currentLetter: 1,
+        upgradeCost: 25,
+        prestigeCost: 5000000,
+        prestigeLevel: 1,
+        metaPrestigeCost: 6,
+        metaPrestigeLevel: 1
+    }
+}
+else{
+    //load saved user data
+    userData = JSON.parse(window.localStorage["gameStateData"]);
+    //push current upgrades
+    let temporaryLetter = userData.currentLetter;
+    userData.currentLetter = 1;
+    pushUpgrade(temporaryLetter - 1);
+    userData.currentLetter = temporaryLetter;
+
+    //update prestige cost
+    aPrestigeCost.innerHTML = userData.prestigeCost + " A";
+    aMetaprestigeCost.innerHTML = userData.metaPrestigeCost + " Prestige";
+}
+
+//global variables
 var upgradeMultiplier = 1.15;
-var prestigeMultiplier = 10;
-var currentLetterIndex = 0;
-var prestigeCost = 5000000;
-var prestigeLevel = 1;
+var prestigeMultiplier = 5;
+var metaprestigeMultiplier = 1.25;
 
 function buttonClick(letter){
     let UpperLetter = letter.toUpperCase();
@@ -39,29 +68,41 @@ function buttonClick(letter){
     const letterToFind = (isLetter) => isLetter == UpperLetter;
 
     let indexOfLetter = progress.findIndex(letterToFind);
-    letters[letter] += ((increaseAmount / (2 ** indexOfLetter)) * prestigeLevel);
+    userData.letters[letter] += ((userData.increaseAmount / (2 ** indexOfLetter)) * (userData.prestigeLevel ** userData.metaPrestigeLevel));
 }
 
 function upgrade(letter){
-    if((letters[letter] >= upgradeCost) && currentLetter < 25){
-        letters[letter] -= upgradeCost;
-        upgradeCost = Math.ceil(upgradeCost * (upgradeMultiplier ** currentLetter));
-        aUpgradeButton.innerHTML = "->" + progress[currentLetter + 1];
-        let lowerCase = progress[currentLetter].toLowerCase();
+    if((userData.letters[letter] >= userData.upgradeCost) && userData.currentLetter < 25){
+        userData.letters[letter] -= userData.upgradeCost;
+        userData.upgradeCost = Math.ceil((userData.upgradeCost * (upgradeMultiplier ** userData.currentLetter)) / userData.metaPrestigeLevel);
+        aUpgradeButton.innerHTML = "->" + progress[userData.currentLetter + 1];
+        let lowerCase = progress[userData.currentLetter].toLowerCase();
         let newUpgrade = "upgrade('" + lowerCase + "')";
         aUpgradeButton.setAttribute("onclick", newUpgrade);
-        aCost.innerHTML = upgradeCost + " " + progress[currentLetter];
-        addLetterDiv(currentLetter);
-        currentLetter++;
-        increaseAmount *= 2;
+        aCost.innerHTML = userData.upgradeCost + " " + progress[userData.currentLetter];
+        addLetterDiv(userData.currentLetter);
+        userData.currentLetter++;
+        userData.increaseAmount *= 2;
     }
-    else if((letters[letter] >= upgradeCost) && currentLetter == 25){
-        letters[letter] -= upgradeCost;
-        upgradeCost = Math.ceil(upgradeCost * (upgradeMultiplier ** currentLetter));
-        addLetterDiv(currentLetter);
+    else if((userData.letters[letter] >= userData.upgradeCost) && userData.currentLetter == 25){
+        userData.letters[letter] -= userData.upgradeCost;
+        userData.upgradeCost = Math.ceil(userData.upgradeCost * (upgradeMultiplier ** userData.currentLetter));
+        addLetterDiv(userData.currentLetter);
         aUpgradeDiv.remove();
-        currentLetter++;
-        increaseAmount *= 2;
+        userData.currentLetter++;
+        userData.increaseAmount *= 2;
+    }
+}
+
+function pushUpgrade(number){
+    for(let x = 0; x < number; x++){
+        aUpgradeButton.innerHTML = "->" + progress[userData.currentLetter + 1];
+        let lowerCase = progress[userData.currentLetter].toLowerCase();
+        let newUpgrade = "upgrade('" + lowerCase + "')";
+        aUpgradeButton.setAttribute("onclick", newUpgrade);
+        aCost.innerHTML = userData.upgradeCost + " " + progress[userData.currentLetter];
+        addLetterDiv(userData.currentLetter);
+        userData.currentLetter++;
     }
 }
 
@@ -89,46 +130,102 @@ function addLetterDiv(index){
 }
 
 function prestige(){
-    if(letters.a >= prestigeCost){
-        increaseAmount = 1;
-        currentLetter = 1;
-        upgradeCost = 25;
-        currentLetterIndex = 0;
-        prestigeLevel++;
+    if(userData.letters.a >= userData.prestigeCost){
+        userData.increaseAmount = 1;
+        userData.currentLetter = 1;
+        userData.upgradeCost = 25;
+        userData.prestigeLevel++;
         buttonDiv.innerHTML = "";
         buttonDiv.appendChild(aButtonDefault);
-        Object.keys(letters).forEach(prop => letters[prop] = 0);
+        Object.keys(userData.letters).forEach(prop => userData.letters[prop] = 0);
         let lowerCase = progress[0].toLowerCase();
         let newUpgrade = "upgrade('" + lowerCase + "')";
         aUpgradeButton.setAttribute("onclick", newUpgrade);
         aUpgradeButton.innerHTML = "->" + progress[1];
-        aCost.innerHTML = upgradeCost + " " + progress[0];
-        prestigeCost = Math.ceil(prestigeCost * (prestigeMultiplier ** prestigeLevel));
-        aPrestigeCost.innerHTML = prestigeCost + " A";
+        aCost.innerHTML = userData.upgradeCost + " " + progress[0];
+        userData.prestigeCost = Math.ceil((userData.prestigeCost * (prestigeMultiplier ** userData.prestigeLevel)) / userData.metaPrestigeLevel);
+        aPrestigeCost.innerHTML = userData.prestigeCost + " A";
     }
-    
+    //cannot afford
+}
+
+function metaprestige(){
+    if(userData.prestigeLevel >= userData.metaPrestigeCost){
+        userData.increaseAmount = 1;
+        userData.currentLetter = 1;
+        userData.upgradeCost = 25;
+        userData.prestigeLevel = 1;
+        userData.prestigeCost = 5000000;
+        userData.metaPrestigeLevel++;
+        buttonDiv.innerHTML = "";
+        buttonDiv.appendChild(aButtonDefault);
+        Object.keys(userData.letters).forEach(prop => userData.letters[prop] = 0);
+        userData.upgradeCost = Math.ceil((userData.upgradeCost * (upgradeMultiplier ** userData.currentLetter)) / userData.metaPrestigeLevel);
+        let lowerCase = progress[0].toLowerCase();
+        let newUpgrade = "upgrade('" + lowerCase + "')";
+        aUpgradeButton.setAttribute("onclick", newUpgrade);
+        aUpgradeButton.innerHTML = "->" + progress[1];
+        aCost.innerHTML = userData.upgradeCost + " " + progress[0];
+        aPrestigeCost.innerHTML = userData.prestigeCost + " A";
+        userData.metaPrestigeCost = Math.ceil((userData.metaPrestigeCost * (metaprestigeMultiplier ** userData.metaPrestigeLevel)));
+        aMetaprestigeCost.innerHTML = userData.metaPrestigeCost + " Prestige";
+    }
 }
 
 window.setInterval(() => {
-    aCount.innerHTML = Math.floor(letters.a);
+    aCount.innerHTML = Math.floor(userData.letters.a);
     for(let x = 0; x < progress.length; x++){
         let lowerLetter = progress[x].toLowerCase();
         let letterId = lowerLetter + "Count";
         if(document.getElementById(letterId)){
             let element = document.getElementById(letterId);
-            element.innerHTML = Math.floor(letters[lowerLetter]);
+            element.innerHTML = Math.floor(userData.letters[lowerLetter]);
         }
     }
+    prestigeCountText.innerHTML = userData.prestigeLevel - 1;
+    metaprestigeCountText.innerHTML = userData.metaPrestigeLevel - 1;
 }, 10);
 
 window.setInterval(() => {
     for(let x = 0; x < progress.length - 1; x++){
         let lowerLetter = progress[x].toLowerCase();
         let nextLetter = progress[x + 1].toLowerCase();
-        letters[lowerLetter] += prestigeLevel * letters[nextLetter];
+        userData.letters[lowerLetter] += userData.prestigeLevel * userData.letters[nextLetter];
     }
+
+    save();
 }, 1000);
 
 function cheat(letter, number){
-    letters[letter] = number;
+    userData.letters[letter] = number;
+}
+
+function save(){
+    userData = {
+        letters: userData.letters,
+        increaseAmount: userData.increaseAmount,
+        currentLetter: userData.currentLetter,
+        upgradeCost: userData.upgradeCost,
+        prestigeCost: userData.prestigeCost,
+        prestigeLevel: userData.prestigeLevel,
+        metaPrestigeCost: userData.metaPrestigeCost,
+        metaPrestigeLevel: userData.metaPrestigeLevel
+    }
+    localStorage.setItem("gameStateData", JSON.stringify(userData));
+}
+
+function reset(){
+    //reset saved user data to default and reload page
+    userData = {
+        letters: {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0},
+        increaseAmount: 1,
+        currentLetter: 1,
+        upgradeCost: 25,
+        prestigeCost: 5000000,
+        prestigeLevel: 1,
+        metaPrestigeCost: 6,
+        metaPrestigeLevel: 1
+    }
+    localStorage.setItem("gameStateData", JSON.stringify(userData));
+    location.reload();
 }
